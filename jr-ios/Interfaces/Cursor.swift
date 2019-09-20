664c85e28c7439684a9f056b2d4d6b723329d156
damien <damien@rooking.co.jp>
2019-09-20 14:23:49 +0900

    feature(refactor) - Organize project with folders

---
+++ jr-ios/Interfaces/Cursor.swift
@@ -0,0 +1,19 @@
+//
+//  Cursor.swift
+//  jr-ios
+//
+//  Created by damien on 2019/09/20.
+//  Copyright © 2019 beacrew. All rights reserved.
+//
+
+import Foundation
+
+class Cursor {
+    var x: Double
+    var y: Double
+    
+    init?(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
---
+++ jr-ios/Interfaces/GuidePlate.swift
@@ -0,0 +1,13 @@
+//
+//  GuidePlate.swift
+//  jr-ios
+//
+//  Created by damien on 2019/09/20.
+//  Copyright © 2019 beacrew. All rights reserved.
+//
+
+import Foundation
+
+class GuidePlate: Decodable {
+    
+}
